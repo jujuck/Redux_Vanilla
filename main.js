5e3232f08647a081db3e5cc61b010fb4f34b4ca0
Julien ichard WCS <julien.y.richard@gmail.com>
2021-12-13 11:30:05 +0100

    adding the initial State

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+console.log("Hello World");
+
+/**
+ * Initial state object
+ *   - filter is set to show all tasks (done and not done)
+ *   - fake tasks have been created
+ */
+const initialState = {
+  filter: 'SHOW_ALL',
+  tasks: [
+    { id: 1, title: 'Learn HTML', done: true },
+    { id: 2, title: 'Learn React', done: false },
+    { id: 3, title: 'Learn Redux', done: false }
+  ]
+};
